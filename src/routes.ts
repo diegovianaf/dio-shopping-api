46f019fa2b5786ee9ed797835e6deac5c16d937a
Diego Viana <diegovianaf7@gmail.com>
2022-07-26 11:42:52 -0300

    add controller messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router, Request, Response } from 'express'
+import { CreateMessageController } from './controllers/CreateMessageController'
+import { ListMessageController } from './controllers/ListMessageController'
 
 const router = Router()
+
+const createMessageController = new CreateMessageController()
+const listMessageController = new ListMessageController()
 
 router.get('/', (request: Request, response: Response) => {
   return response.json({ message: 'Welcome to the DIO Shopping API' })
 })
+
+router.get('/message', listMessageController.handle)
+router.post('/message', createMessageController.handle)
 
 export { router }
